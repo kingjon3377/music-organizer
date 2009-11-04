627ccb870d0cca13b6573a739f61bf5eebb9f1b4
Jonathan Lovelace <kingjon3377@gmail.com>
2009-11-04 10:46:47 -0500

    Add an interface to edit a tune's name

--- view/TunePanel.java
+++ view/TunePanel.java
@@ -2,7 +2,9 @@ package view;
 
 import java.awt.GridLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * A panel to create and edit tunes.
@@ -14,11 +16,16 @@ public class TunePanel extends JPanel {
 	 * Version UID for serialization.
 	 */
 	private static final long serialVersionUID = -1818025890818661076L;
-
+	/**
+	 * Text box for the tune name
+	 */
+	private final JTextField nameBox = new JTextField();
 	/**
 	 * Constructor.
 	 */
 	public TunePanel() {
 		super(new GridLayout(0, 2));
+		add(new JLabel("Tune name"));
+		add(nameBox);
 	}
 }
