f2bec277277eba9a5ca5d7355b7c33763a23e6dc
Jonathan Lovelace <kingjon3377@gmail.com>
2009-11-04 11:12:17 -0500

    A JButton subclass that takes an ActionListener as a constructor parameter

---
+++ utils/ListenerButton.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+/**
+ * A JButton that takes its ActionListener as a constructor parameter
+ * 
+ * @author kingjon
+ */
+public class ListenerButton extends JButton {
+	/**
+	 * Version UID for serialization
+	 */
+	private static final long serialVersionUID = 7050897114145426560L;
+
+	/**
+	 * @param string
+	 *            The button string
+	 * @param listener
+	 *            The listener to add
+	 */
+	public ListenerButton(final String string, final ActionListener listener) {
+		super(string);
+		addActionListener(listener);
+	}
+}
