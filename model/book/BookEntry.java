8ab7e94f1d7c96f9b8c58745c5619d529202081e
Jonathan Lovelace <1522278+kingjon3377@users.noreply.github.com>
2009-11-10 14:39:44 -0500

    BookEntry: Added a page-number field and a toString method, and made the Tune member non-final.

--- model/book/BookEntry.java
+++ model/book/BookEntry.java
@@ -10,19 +10,50 @@ public class BookEntry {
 	/**
 	 * The tune
 	 */
-	private final Tune tune;
+	private Tune tune;
+	/**
+	 * The page number the tune is on
+	 */
+	private int page;
 	/**
 	 * Constructor
 	 * @param theTune The tune in this entry
 	 */
 	public BookEntry(final Tune theTune) {
 		tune = theTune;
+		page = 0;
 	}
 	/**
-	 * Accessor
+	 * Accessor.
 	 * @return the tune in the entry
 	 */
 	public final Tune getTune() {
 		return tune;
+	}
+	/**
+	 * Mutator.
+	 * @param newTune the new tune in the entry
+	 */
+	public void setTune(final Tune newTune) {
+		tune = newTune;
+	}
+	/**
+	 * @return the page the tune is on
+	 */
+	public int getPage() {
+		return page;
+	}
+	/**
+	 * @param pageNum the page the tune is on
+	 */
+	public void setPage(final int pageNum) {
+		page = pageNum;
+	}
+	/**
+	 * @return a String representation of the entry
+	 */
+	@Override
+	public String toString() {
+		return (tune == null ? "No tune" : tune.toString()) + " on page " + page;
 	}
 }
